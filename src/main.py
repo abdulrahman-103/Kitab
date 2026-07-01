74e2ae4fa84d32e174ef5d361b6f50fefaee1c84
Abdulrahman <noemail@example.com>
2026-07-02 02:11:29 +0300

    preparing for packaging

--- src/main.py
+++ src/main.py
@@ -7,15 +7,11 @@ from PySide6.QtWidgets import QApplication
 from mainwindow import MainWindow
 import sys
 
-if sys.platform == "win32":
-    import ctypes
-    console_window = ctypes.windll.kernel32.GetConsoleWindow()
-    if console_window != 0:
-        ctypes.windll.user32.ShowWindow(console_window, 0)
+def main():
+    app = QApplication(sys.argv)
+    screen_resolution = app.primaryScreen().availableSize()
+    window = MainWindow()
+    app.exec()
 
-
-app = QApplication(sys.argv)
-screen_resolution = app.primaryScreen().availableSize()
-window = MainWindow()
-
-app.exec()
+if __name__ == "__main__":
+    main()
