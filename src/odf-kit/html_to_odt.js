15677de8f7fe495b2f7b98c875cfebada1511199
Abdulrahman <176532798+abdulrahman-103@users.noreply.github.com>
2026-07-25 15:32:43 +0300

    move odt-kit bridges from src/odf-kit to src/kitab

--- src/odf-kit/html_to_odt.js
+++
@@ -1,6 +0,0 @@
-import { htmlToOdt } from "./odt.js";
-import { readFileSync, writeFileSync } from "fs";
-const outputFilename = process.argv[2];
-const html = readFileSync(0, "utf-8");
-const odt_data = await htmlToOdt(html);
-writeFileSync(outputFilename, odt_data);
--- src/odf-kit/odt_to_html.js
+++
@@ -1,6 +0,0 @@
-import { odtToHtml } from "./reader.js";
-import { readFileSync } from "fs";
-const inputFilename = process.argv[2];
-const bytes = new Uint8Array(readFileSync(inputFilename));
-const html = odtToHtml(bytes);
-process.stdout.write(html);
