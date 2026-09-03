0efe526b3fc9431922cbaa4cebf06b18b22a47bf
Abdulrahman <176532798+abdulrahman-103@users.noreply.github.com>
2026-09-03 19:24:03 +0300

    some compile stuff

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-nim c --app:lib --out:src/kitab/parser.so --verbosity:0 --hints:off src/parser/parser.nim
+nim c --app:lib --out:src/kitab/kitab_parser.so --verbosity:0 --hints:off src/parser/kitab_parser.nim
